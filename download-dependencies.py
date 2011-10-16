bfe56169bd0962fc7a6280ba9b0ce253b32f7021
Shimon Rura <shimon@rura.org>
2011-10-16 19:17:48 -0400

    better handling of submodules within dependency packages

--- download-dependencies.py
+++ download-dependencies.py
@@ -4,27 +4,56 @@ import os
 
 DEPS = [
     dict(name="django-nonrel",
+         packages=["django"],
          src="http://bitbucket.org/wkornewald/django-nonrel/get/tip.zip"),
     dict(name="djangoappengine",
+         packages=["."],
          src="http://bitbucket.org/wkornewald/djangoappengine/get/tip.zip"),
     dict(name="djangotoolbox",
+         packages=["djangotoolbox"],
          src="http://bitbucket.org/wkornewald/djangotoolbox/get/tip.zip"),
     dict(name="django-autoload",
+         packages=["autoload"],
          src="http://bitbucket.org/twanschik/django-autoload/get/tip.zip"),
     dict(name="django-dbindexer",
+         packages=["dbindexer"],
          src="http://bitbucket.org/wkornewald/django-dbindexer/get/tip.zip"),
     ]
 
+LIB_DIR = "lib"
+
 if __name__ == "__main__":
-    for d in DEPS:
-        name = d["name"]
-        src = d["src"]
-        if os.path.exists(name):
-            print "%s: already installed." % name
-        else:
-            print "%s: downloading..." % name
-            os.system("wget -c -O '%s.zip' '%s'" % (name, src))
-            print "%s: unzipping..." % name
-            os.system("unzip -q '%s.zip'" % name)
-            print "%s: renaming..." % name
-            os.system("mv -v *-%s-tip %s" % (name, name))
+
+    curdir = os.getcwd()
+
+    if not os.path.isdir(LIB_DIR):
+        os.makedirs(LIB_DIR)
+    os.chdir(LIB_DIR)
+
+    try:
+        for d in DEPS:
+            name = d["name"]
+            src = d["src"]
+            packages = d["packages"] or []
+
+            if os.path.exists(name):
+                print "%s: already downloaded." % name
+            else:
+                print "%s: downloading..." % name
+                os.system("wget -c -O '%s.zip' '%s'" % (name, src))
+                print "%s: unzipping..." % name
+                os.system("unzip -q '%s.zip'" % name)
+                print "%s: renaming..." % name
+                os.system("mv -v *-%s-tip %s" % (name, name))
+
+            for packagedir in packages:
+                print "%s: linking %s..." % (name, packagedir)
+                if packagedir == ".":
+                    source = os.path.join(LIB_DIR, name)
+                else:
+                    source = os.path.join(LIB_DIR, name, packagedir)
+
+                os.system("(cd ..; ln -vsf %s .)" % source)
+
+    finally:
+        os.chdir(curdir)
